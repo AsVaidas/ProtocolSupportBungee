c1c45da5a910f38fff93ad5f12bb1eb4845508d2
Colin Godsey <crgodsey@gmail.com>
2019-02-16 08:34:22 -0700

    new metrics

--- src/protocolsupport/injector/pe/PERakNetMetrics.java
+++ src/protocolsupport/injector/pe/PERakNetMetrics.java
@@ -11,9 +11,9 @@ import raknetserver.RakNetServer;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class PERakNetMetrics implements RakNetServer.Metrics {
+public class PERakNetMetrics implements RakNetServer.MetricsLogger {
     private static double nsInS = 1.0 / TimeUnit.NANOSECONDS.convert(1, TimeUnit.SECONDS);
-    final static RakNetServer.Metrics INSTANCE = new PERakNetMetrics();
+    static final PERakNetMetrics INSTANCE = new PERakNetMetrics();
 
     static {
         final int port = Utils.getJavaPropertyValue("metrics-port", 1271, Integer::parseInt);
@@ -26,77 +26,79 @@ public class PERakNetMetrics implements RakNetServer.Metrics {
         }
     }
 
-    final Counter outPacket = Counter.build().name("out_packet").help("Game packets sent").register();
-    final Counter inPacket = Counter.build().name("in_packet").help("Game packets received").register();
-    final Counter join = Counter.build().name("join").help("Outbound packet joins").register();
-    final Counter send = Counter.build().name("send").help("Outbound packets sent").register();
-    final Counter recv = Counter.build().name("recv").help("Outbound packets received").register();
-    final Counter resend = Counter.build().name("resend").help("Outbound packet resends").register();
-    final Counter ackSend = Counter.build().name("ack_send").help("ACKs sent").register();
-    final Counter nackSend = Counter.build().name("nack_send").help("NACKs sent").register();
-    final Counter ackRecv = Counter.build().name("ack_recv").help("ACKs received").register();
-    final Counter nackRecv = Counter.build().name("nack_recv").help("NACKs received").register();
-    final Histogram sendAttempts = Histogram.build().name("send_attempts").help("Number of send attempts before ACK")
-            .buckets(0.99, 1.99, 2.99, 3.99, 4.99, 5.99, 6.99, 7.99, 8.99, 9.99, 10.99, 15, 20).register();
+    final Counter packetsIn = Counter.build().name("packetsIn").help("packetsIn").register();
+    final Counter framesIn = Counter.build().name("framesIn").help("framesIn").register();
+    final Counter bytesIn = Counter.build().name("bytesIn").help("bytesIn").register();
+    final Counter packetsOut = Counter.build().name("packetsOut").help("packetsOut").register();
+    final Counter framesOut = Counter.build().name("framesOut").help("framesOut").register();
+    final Counter bytesOut = Counter.build().name("bytesOut").help("bytesOut").register();
+    final Counter bytesRecalled = Counter.build().name("bytesRecalled").help("bytesRecalled").register();
+    final Counter bytesACKd = Counter.build().name("bytesACKd").help("bytesACKd").register();
+    final Counter bytesNACKd = Counter.build().name("bytesNACKd").help("bytesNACKd").register();
+    final Counter acksSent = Counter.build().name("acksSent").help("acksSent").register();
+    final Counter nacksSent = Counter.build().name("nacksSent").help("nacksSent").register();
     final Histogram rtt = Histogram.build().name("rtt").help("RTT in seconds").register();
+    final Counter outPrePacket = Counter.build().name("out_pre_comp_packet").help("Game packets sent (pre compression)").register();
+    public final Counter outPreComp = Counter.build().name("out_pre_comp").help("Bytes out pre-compression").register();
+    public final Counter outPostComp = Counter.build().name("out_post_comp").help("Bytes out post-compression").register();
 
     private PERakNetMetrics() {
 
     }
 
     @Override
-    public void incrOutPacket(int i) {
-        outPacket.inc(i);
+    public void packetsIn(int i) {
+        packetsIn.inc(i);
     }
 
     @Override
-    public void incrInPacket(int i) {
-        inPacket.inc(i);
+    public void framesIn(int i) {
+        framesIn.inc(i);
     }
 
     @Override
-    public void incrJoin(int i) {
-        join.inc(i);
+    public void bytesIn(int i) {
+        bytesIn.inc(i);
     }
 
     @Override
-    public void incrSend(int i) {
-        send.inc(i);
+    public void packetsOut(int i) {
+        packetsOut.inc(i);
     }
 
     @Override
-    public void incrRecv(int i) {
-        recv.inc(i);
+    public void framesOut(int i) {
+        framesOut.inc(i);
     }
 
     @Override
-    public void incrResend(int i) {
-        resend.inc(i);
+    public void bytesOut(int i) {
+        bytesOut.inc(i);
     }
 
     @Override
-    public void incrAckSend(int i) {
-        ackSend.inc(i);
+    public void bytesRecalled(int i) {
+        bytesRecalled.inc(i);
     }
 
     @Override
-    public void incrNackSend(int i) {
-        nackSend.inc(i);
+    public void bytesACKd(int i) {
+        bytesACKd.inc(i);
     }
 
     @Override
-    public void incrAckRecv(int i) {
-        ackRecv.inc(i);
+    public void bytesNACKd(int i) {
+        bytesNACKd.inc(i);
     }
 
     @Override
-    public void incrNackRecv(int i) {
-        nackRecv.inc(i);
+    public void acksSent(int i) {
+        acksSent.inc(i);
     }
 
     @Override
-    public void measureSendAttempts(int i) {
-        sendAttempts.observe(i);
+    public void nacksSent(int i) {
+        nacksSent.inc(i);
     }
 
     @Override
